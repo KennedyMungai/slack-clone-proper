7c656fc7b2a5f8ba26baad41b2eec3f9ff283b18
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-19 10:02:46 +0300

    Made some changes to the project index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import { AuthScreen } from "@/features/auth/components/auth-screen";
 
 const HomePage = () => {
-	return <AuthScreen />;
+	return (
+    <main className="flex h-full items-center justify-center bg-[#5C3B58]">
+      <AuthScreen />
+    </main>
+  );
 };
 
 export default HomePage;
