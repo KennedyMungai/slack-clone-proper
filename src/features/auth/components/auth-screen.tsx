1e40a09d4771f19669436818be04a6f6481a5d7f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-19 10:03:11 +0300

    Added some styling to the Auth Screen component

--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -1,5 +1,14 @@
 "use client";
 
+import { useState } from "react";
+import { SignInFlow } from "@/features/auth/types";
+
 export const AuthScreen = () => {
-	return <div className="h-full bg-indigo-600 text-7xl">AuthScreen</div>;
+  const [state, setState] = useState<SignInFlow>("signIn");
+
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="md:h-auto md:w-[420px]">Auth Screen</div>
+    </div>
+  );
 };
