2428fc79a4f9fd0d637fefe092703fad5229037c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-19 14:40:13 +0300

    Added the user button to the index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import UserButton from "@/features/auth/components/user-button";
+
 const HomePage = () => {
-  return <main className="h-full">Main Page</main>;
+  return (
+    <main className="h-full">
+      <UserButton />
+    </main>
+  );
 };
 
 export default HomePage;
