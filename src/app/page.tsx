791abbe46dc57ff8c41d2a8b83898714a7abe25b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-19 11:28:13 +0300

    Some auth page shenanigans

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { AuthScreen } from "@/features/auth/components/auth-screen";
 const HomePage = () => {
 	return (
     <main className="flex h-full items-center justify-center bg-[#5C3B58]">
-      <AuthScreen />
+      Main Page
     </main>
   );
 };
