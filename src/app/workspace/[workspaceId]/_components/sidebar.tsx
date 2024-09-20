7fcb182528da9d803c5505b33b8868853e09c33d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-20 10:58:57 +0300

    Added the mode toggle component

--- src/app/workspace/[workspaceId]/_components/sidebar.tsx
+++ src/app/workspace/[workspaceId]/_components/sidebar.tsx
@@ -1,12 +1,14 @@
 import UserButton from "@/features/auth/components/user-button";
 import WorkspaceSwitcher from "./workspace-switcher";
+import { ModeToggle } from "@/components/mode-toggle";
 
 const Sidebar = () => {
   return (
     <aside className="flex h-full w-[70px] flex-col items-center gap-y-4 bg-[#481349] pb-4 pt-[9px]">
       <WorkspaceSwitcher />
-      <div className="mt-auto flex flex-col items-center justify-center gap-y-1">
+      <div className="mt-auto flex flex-col items-center justify-center gap-y-1 space-y-4">
         <UserButton />
+        <ModeToggle />
       </div>
     </aside>
   );
