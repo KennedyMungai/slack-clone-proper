8f43d567c035eaa4c269b31e8e6b874f9340acce
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-20 09:52:36 +0300

    Using the hook to fetch workspace data

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 
 const Workspace = () => {
   const workspaceId = useWorkspaceId();
 
-  return <div>{workspaceId}</div>;
+  const { data } = useGetWorkspace({ id: workspaceId });
+
+  return <div>{JSON.stringify(data)}</div>;
 };
 
 export default Workspace;
