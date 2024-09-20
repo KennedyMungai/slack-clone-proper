568a2e43c2a4efe7d9c5ef1bea185767fca3d9e1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-20 09:24:35 +0300

    Created the create workspace modal

--- src/features/workspaces/components/create-workspace-modal.tsx
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -8,15 +8,33 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
+import { useCreateWorkspace } from "@/features/workspaces/api/use-create-workspace";
 import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
 
 const CreateWorkspaceModal = () => {
   const [open, setOpen] = useCreateWorkspaceModal();
+
+  const [name, setName] = useState("");
+
+  const router = useRouter();
+
+  const { mutate, isPending } = useCreateWorkspace();
 
   const handleClose = () => {
     setOpen(false);
 
     // TODO: Clear form
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    mutate(
+      { name },
+      { onSuccess: (data) => router.push(`/workspaces/${data}`) },
+    );
   };
 
   return (
@@ -25,17 +43,20 @@ const CreateWorkspaceModal = () => {
         <DialogHeader>
           <DialogTitle>Add a workspace</DialogTitle>
         </DialogHeader>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={handleSubmit}>
           <Input
-            disabled={false}
-            value={""}
+            disabled={isPending}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             required
             autoFocus
             minLength={3}
             placeholder="Workspace Name e.g. 'Work', 'Personal', 'Home'"
           />
           <div className="mt-2 flex h-full justify-end">
-            <Button disabled={false}>Create</Button>
+            <Button disabled={isPending} type="submit">
+              Create
+            </Button>
           </div>
         </form>
       </DialogContent>
