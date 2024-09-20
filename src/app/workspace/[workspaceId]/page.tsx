cb079139279a6b02a97528b4b65fd0aeed70a50c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-20 09:20:11 +0300

    Created the workspace page

---
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  params: {
+    workspaceId: string;
+  };
+};
+
+const Workspace = ({ params: { workspaceId } }: Props) => {
+  return <div>{workspaceId}</div>;
+};
+
+export default Workspace;
