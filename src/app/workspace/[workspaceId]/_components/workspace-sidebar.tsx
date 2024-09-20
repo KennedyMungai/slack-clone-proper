1231bc40398d827fa351b68c839253e030e27c92
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-20 14:40:32 +0300

    Added a resizable panel for the workspace sidevar and the children

--- src/app/workspace/[workspaceId]/_components/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/_components/workspace-sidebar.tsx
@@ -1,7 +1,46 @@
-type Props = {};
+"use client";
+
+import { useCurrentMember } from "@/features/members/api/use-current-member";
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { AlertTriangleIcon, LoaderIcon } from "lucide-react";
+import WorkspaceHeader from "./workspace-header";
 
 const WorkspaceSidebar = () => {
-  return <div>WorkspaceSidebar</div>;
+  const workspaceId = useWorkspaceId();
+
+  const { data: member, isLoading: isMemberLoading } = useCurrentMember({
+    workspaceId,
+  });
+  const { data: workspace, isLoading: isWorkspaceLoading } = useGetWorkspace({
+    id: workspaceId,
+  });
+
+  if (isWorkspaceLoading || isMemberLoading) {
+    return (
+      <div className="flex h-full flex-col items-center justify-center bg-[#5E2C5F]">
+        <LoaderIcon className="size-5 animate-spin text-white" />
+      </div>
+    );
+  }
+
+  if (!workspace || !member) {
+    return (
+      <div className="flex h-full flex-col items-center justify-center gap-y-2 bg-[#5E2C5F]">
+        <AlertTriangleIcon className="size-5 text-white" />
+        <p className="text-sm text-white">Workspace not found</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex h-full flex-col gap-y-2 bg-[#5E2C5F]">
+      <WorkspaceHeader
+        workspace={workspace}
+        isAdmin={member.role === "admin"}
+      />
+    </div>
+  );
 };
 
 export default WorkspaceSidebar;
