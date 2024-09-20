46ee6ba070a67e4b0b953675ba7ef7ed8e2faed2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-20 13:11:00 +0300

    Made some changes to the current members query

--- convex/members.ts
+++ convex/members.ts
@@ -2,23 +2,22 @@ import { v } from "convex/values";
 import { query } from "./_generated/server";
 import { auth } from "./auth";
 
-const current = () =>
-  query({
-    args: { workspaceId: v.id("workspaces") },
-    handler: async (ctx, args) => {
-      const userId = await auth.getUserId(ctx);
+export const current = query({
+  args: { workspaceId: v.id("workspaces") },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
 
-      if (!userId) return null;
+    if (!userId) return null;
 
-      const member = await ctx.db
-        .query("members")
-        .withIndex("by_workspace_id_user_id", (q) =>
-          q.eq("workspaceId", args.workspaceId).eq("userId", userId),
-        )
-        .collect();
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId),
+      )
+      .collect();
 
-      if (!member) return null;
+    if (!member) return null;
 
-      return member;
-    },
-  });
+    return member;
+  },
+});
