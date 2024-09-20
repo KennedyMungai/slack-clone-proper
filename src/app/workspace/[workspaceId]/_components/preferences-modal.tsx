c0d5e24f3c108910ff8dc71779ab5aca4ee1a5a5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-20 15:29:36 +0300

    Added some markup and styling to  the preferences modal

--- src/app/workspace/[workspaceId]/_components/preferences-modal.tsx
+++ src/app/workspace/[workspaceId]/_components/preferences-modal.tsx
@@ -6,6 +6,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { TrashIcon } from "lucide-react";
 import { useState } from "react";
 
 type Props = {
@@ -19,10 +20,29 @@ const PreferencesModal = ({ initialValue, open, setOpen }: Props) => {
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent>
-        <DialogHeader>
+      <DialogContent className="overflow-hidden bg-gray-50 p-0">
+        <DialogHeader className="border-b bg-white p-4">
           <DialogTitle>{value}</DialogTitle>
         </DialogHeader>
+        <div className="flex flex-col gap-y-2 px-4 pb-4">
+          <div className="cursor-pointer rounded-lg border bg-white px-5 py-4 hover:bg-gray-50">
+            <div className="flex items-center justify-between">
+              <p className="text-sm font-semibold">Workspace Name</p>
+              <p className="text-sm font-semibold text-[#1264A3] hover:underline">
+                Edit
+              </p>
+            </div>
+            <p className="text-sm">{value}</p>
+          </div>
+          <button
+            disabled={false}
+            onClick={() => {}}
+            className="flex cursor-pointer items-center gap-x-2 rounded-lg border bg-white px-5 py-4 text-rose-600 hover:bg-gray-50"
+          >
+            <TrashIcon className="size-4" />
+            <p className="text-sm font-semibold">Delete Workspace</p>
+          </button>
+        </div>
       </DialogContent>
     </Dialog>
   );
