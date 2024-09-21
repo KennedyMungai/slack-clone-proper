9fe70c8eb9264f04e194ca4eaf0d07f5df35a10b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-21 11:13:41 +0300

    Implemented different variants of the sidebar item

--- src/app/workspace/[workspaceId]/_components/sidebar-item.tsx
+++ src/app/workspace/[workspaceId]/_components/sidebar-item.tsx
@@ -2,21 +2,41 @@
 
 import { Button } from "@/components/ui/button";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
 import { LucideIcon } from "lucide-react";
 import Link from "next/link";
 import { IconType } from "react-icons/lib";
+
+const sidebarItemVariants = cva(
+  "flex items-center gap-1.5 justify-center font-normal h-7 px-[18px] text-sm overflow-hidden",
+  {
+    variants: {
+      variant: {
+        default: "text-[#f9edffcc]",
+        active: "text-[#481349] bg-white/90 hover:bg-white/90",
+      },
+    },
+  },
+);
 
 type Props = {
   label: string;
   icon: LucideIcon | IconType;
   id: string;
+  variant?: VariantProps<typeof sidebarItemVariants>["variant"];
 };
 
-const SidebarItem = ({ icon: Icon, id, label }: Props) => {
+const SidebarItem = ({ icon: Icon, id, label, variant }: Props) => {
   const workspaceId = useWorkspaceId();
 
   return (
-    <Button asChild variant="transparent" size={"sm"}>
+    <Button
+      asChild
+      variant="transparent"
+      size={"sm"}
+      className={cn(sidebarItemVariants({ variant }))}
+    >
       <Link href={`/workspace/${workspaceId}/channel/${id}`}>
         <Icon className="mr-2 size-5" />
         <span className="text-base">{label}</span>
