a736b1c2610af48d7781407a3bd215c5e2a83280
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-21 10:48:49 +0300

    Created the use confirm hook

---
+++ src/hooks/use-confirm.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useState } from "react";
+
+type Props = {
+  title: string;
+  message: string;
+};
+
+const useConfirm = ({
+  title,
+  message,
+}: Props): [() => JSX.Element, () => Promise<unknown>] => {
+  const [promise, setPromise] = useState<{
+    resolve: (value: boolean) => void;
+  } | null>(null);
+
+  const confirm = () =>
+    new Promise((resolve) => {
+      setPromise({ resolve });
+    });
+
+  const handleClose = () => setPromise(null);
+
+  const handleCancel = () => {
+    promise?.resolve(false);
+
+    handleClose();
+  };
+
+  const handleConfirm = () => {
+    promise?.resolve(true);
+
+    handleClose();
+  };
+
+  const ConfirmDialog = () => (
+    <Dialog open={promise !== null}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{message}</DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="pt-2">
+          <Button onClick={handleCancel} variant={"outline"}>
+            Cancel
+          </Button>
+          <Button onClick={handleConfirm} variant={"default"}>
+            Confirm
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+
+  return [ConfirmDialog, confirm];
+};
+
+export default useConfirm;
