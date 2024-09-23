8d0c6d8f76a108cebb1349204d6053c97e6af5b1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-23 11:33:51 +0300

    Added the Verification Input component to the join page

--- src/app/join/[workspaceId]/page.tsx
+++ src/app/join/[workspaceId]/page.tsx
@@ -1,13 +1,39 @@
 "use client";
 
+import Image from "next/image";
 import { useParams } from "next/navigation";
+import VerificationInput from "react-verification-input";
 
 const JoinPage = () => {
   const params = useParams();
 
   const { workspaceId } = params as { workspaceId: string };
 
-  return <div>{workspaceId}</div>;
+  return (
+    <div className="flex h-full flex-col items-center justify-center gap-y-8 rounded-lg bg-white p-8 shadow-sm">
+      <Image src="/logo.svg" width={60} height={60} alt="Logo" />
+      <div className="flex max-w-md flex-col items-center justify-center gap-y-4">
+        <div className="flex flex-col items-center justify-center gap-y-2">
+          <h1 className="text-2xl font-bold">Join Workspace</h1>
+          <p className="text-md text-muted-foreground">
+            Enter the workspace code to join
+          </p>
+        </div>
+        <VerificationInput
+          classNames={{
+            container: "flex gap-x-2",
+            character:
+              "uppercase h-auto rounded-md border border-gray-30 flex items-center justify-center text-lg font-medium text-gray-500",
+            characterInactive: "bg-muted",
+            characterSelected: "bg-white text-black",
+            characterFilled: "bg-white text-black",
+          }}
+          autoFocus
+          length={6}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default JoinPage;
