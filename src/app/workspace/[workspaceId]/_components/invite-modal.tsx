f7f3fe4662737ce7eac924ccae4a00fdd8ebced4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-23 11:04:11 +0300

    Implemented a confirm dialog before a new code is generated

--- src/app/workspace/[workspaceId]/_components/invite-modal.tsx
+++ src/app/workspace/[workspaceId]/_components/invite-modal.tsx
@@ -10,6 +10,7 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { useNewJoinCode } from "@/features/workspaces/api/use-new-join-code";
+import useConfirm from "@/hooks/use-confirm";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { CopyIcon, RefreshCcwIcon } from "lucide-react";
 import { toast } from "sonner";
@@ -24,6 +25,12 @@ type Props = {
 const InviteModal = ({ open, setOpen, joinCode, name }: Props) => {
   const workspaceId = useWorkspaceId();
 
+  const [ConfirmDialog, confirm] = useConfirm({
+    title: "Are you sure?",
+    message:
+      "This will deactivate the current invite code and generate a new one",
+  });
+
   const { mutate, isPending } = useNewJoinCode();
 
   const handleCopy = () => {
@@ -34,7 +41,11 @@ const InviteModal = ({ open, setOpen, joinCode, name }: Props) => {
       .then(() => toast.success("Invite link copied to clipboard"));
   };
 
-  const handleNewCode = () => {
+  const handleNewCode = async () => {
+    const ok = await confirm();
+
+    if (!ok) return;
+
     mutate(
       { workspaceId },
       {
@@ -45,43 +56,46 @@ const InviteModal = ({ open, setOpen, joinCode, name }: Props) => {
   };
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Invite people to {name}</DialogTitle>
-          <DialogDescription>
-            Use the code below to invite people to your workspace
-          </DialogDescription>
-        </DialogHeader>
-        <div className="flex flex-col items-center justify-center gap-y-4 py-10">
-          <p className="text-center text-4xl font-bold uppercase tracking-widest">
-            {joinCode}
-          </p>
-          <Button
-            className="space-x-4"
-            variant={"ghost"}
-            size="sm"
-            onClick={handleCopy}
-            disabled={isPending}
-          >
-            <span>Copy link</span> <CopyIcon className="size-5" />
-          </Button>
-        </div>
-        <div className="flex w-full items-center justify-between">
-          <Button
-            onClick={handleNewCode}
-            variant={"outline"}
-            disabled={isPending}
-          >
-            New Code
-            <RefreshCcwIcon className="ml-2 size-4" />
-          </Button>
-          <DialogClose asChild>
-            <Button disabled={isPending}>Close</Button>
-          </DialogClose>
-        </div>
-      </DialogContent>
-    </Dialog>
+    <>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Invite people to {name}</DialogTitle>
+            <DialogDescription>
+              Use the code below to invite people to your workspace
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col items-center justify-center gap-y-4 py-10">
+            <p className="text-center text-4xl font-bold uppercase tracking-widest">
+              {joinCode}
+            </p>
+            <Button
+              className="space-x-4"
+              variant={"ghost"}
+              size="sm"
+              onClick={handleCopy}
+              disabled={isPending}
+            >
+              <span>Copy link</span> <CopyIcon className="size-5" />
+            </Button>
+          </div>
+          <div className="flex w-full items-center justify-between">
+            <Button
+              onClick={handleNewCode}
+              variant={"outline"}
+              disabled={isPending}
+            >
+              New Code
+              <RefreshCcwIcon className="ml-2 size-4" />
+            </Button>
+            <DialogClose asChild>
+              <Button disabled={isPending}>Close</Button>
+            </DialogClose>
+          </div>
+        </DialogContent>
+      </Dialog>
+      <ConfirmDialog />
+    </>
   );
 };
 
