ed3b620fb7021f9944fb14c0cf69df368b982e85
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-23 10:41:22 +0300

    Added the join code and invite link functionality

--- src/app/workspace/[workspaceId]/_components/invite-modal.tsx
+++ src/app/workspace/[workspaceId]/_components/invite-modal.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { CopyIcon } from "lucide-react";
+import { toast } from "sonner";
 
 type Props = {
   open: boolean;
@@ -13,12 +18,38 @@ type Props = {
 };
 
 const InviteModal = ({ open, setOpen, joinCode, name }: Props) => {
+  const workspaceId = useWorkspaceId();
+
+  const handleCopy = () => {
+    const inviteLink = window.location.origin + `/join/${workspaceId}`;
+
+    navigator.clipboard
+      .writeText(inviteLink)
+      .then(() => toast.success("Invite link copied to clipboard"));
+  };
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Invite people to {name}</DialogTitle>
+          <DialogDescription>
+            Use the code below to invite people to your workspace
+          </DialogDescription>
         </DialogHeader>
+        <div className="flex flex-col items-center justify-center gap-y-4 py-10">
+          <p className="text-center text-4xl font-bold uppercase tracking-widest">
+            {joinCode}
+          </p>
+          <Button
+            className="space-x-4"
+            variant={"ghost"}
+            size="sm"
+            onClick={handleCopy}
+          >
+            <span>Copy link</span> <CopyIcon className="size-5" />
+          </Button>
+        </div>
       </DialogContent>
     </Dialog>
   );
