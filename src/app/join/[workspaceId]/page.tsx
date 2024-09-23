466e0dff38f104f413b7cd49a610362dc8ee9db5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-23 11:48:15 +0300

    Added an image loader on the loading state

--- src/app/join/[workspaceId]/page.tsx
+++ src/app/join/[workspaceId]/page.tsx
@@ -1,20 +1,41 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { useGetWorkspaceInfo } from "@/features/workspaces/api/use-get-workspace-info";
 import Image from "next/image";
-import { useParams } from "next/navigation";
+import Link from "next/link";
 import VerificationInput from "react-verification-input";
+import { Id } from "../../../../convex/_generated/dataModel";
 
-const JoinPage = () => {
-  const params = useParams();
+type Props = {
+  params: { workspaceId: string };
+};
 
-  const { workspaceId } = params as { workspaceId: string };
+const JoinPage = ({ params: { workspaceId } }: Props) => {
+  const { data, isLoading } = useGetWorkspaceInfo({
+    id: workspaceId as Id<"workspaces">,
+  });
+
+  if (isLoading) {
+    return (
+      <div className="flex h-full items-center justify-center bg-white">
+        <Image
+          src="/logo.svg"
+          width={200}
+          height={200}
+          alt="Logo"
+          className="animate-pulse"
+        />
+      </div>
+    );
+  }
 
   return (
     <div className="flex h-full flex-col items-center justify-center gap-y-8 rounded-lg bg-white p-8 shadow-sm">
       <Image src="/logo.svg" width={60} height={60} alt="Logo" />
       <div className="flex max-w-md flex-col items-center justify-center gap-y-4">
         <div className="flex flex-col items-center justify-center gap-y-2">
-          <h1 className="text-2xl font-bold">Join Workspace</h1>
+          <h1 className="text-2xl font-bold">Join {data?.name}</h1>
           <p className="text-md text-muted-foreground">
             Enter the workspace code to join
           </p>
@@ -31,6 +52,11 @@ const JoinPage = () => {
           autoFocus
           length={6}
         />
+      </div>
+      <div className="flex gap-x-4">
+        <Button size="lg" variant={"outline"} asChild>
+          <Link href="/">Back to home</Link>
+        </Button>
       </div>
     </div>
   );
