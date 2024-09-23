a0c114859803565ea9860c2225533cf3d47689a2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-23 09:54:54 +0300

    Added some more markup and logic to the create channel modal

--- src/features/channels/components/create-channel-modal.tsx
+++ src/features/channels/components/create-channel-modal.tsx
@@ -1,22 +1,52 @@
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
-
-type Props = {};
+import { ChangeEvent, useState } from "react";
 
 const CreateChannelModal = () => {
   const [open, setOpen] = useCreateChannelModal();
 
+  const [name, setName] = useState("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.replace(/\s+/g, "-").toLowerCase();
+    setName(value);
+  };
+
+  const handleClose = () => {
+    setName("");
+    setOpen(false);
+  };
+
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
+    <Dialog open={open} onOpenChange={handleClose}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Add a channel</DialogTitle>
         </DialogHeader>
+        <form className="space-y-4">
+          <Input
+            value={name}
+            disabled={false}
+            onChange={(e) => handleChange(e)}
+            required
+            autoFocus
+            minLength={3}
+            maxLength={80}
+            placeholder="e.g. plan-budget"
+          />
+          <div className="flex justify-end">
+            <Button type="submit" disabled={false}>
+              Create
+            </Button>
+          </div>
+        </form>
       </DialogContent>
     </Dialog>
   );
