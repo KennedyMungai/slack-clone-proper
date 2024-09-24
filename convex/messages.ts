c570bff90a61c2871ed7f6e126fdfac62fdecd77
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-24 15:30:05 +0300

    Added the rest of the populateThread function

--- convex/messages.ts
+++ convex/messages.ts
@@ -34,6 +34,23 @@ const populateThread = async (ctx: QueryCtx, messageId: Id<"messages">) => {
       q.eq("parentMessageId", messageId),
     )
     .collect();
+
+    if (messages.length === 0)
+      return { count: 0, image: undefined, timestamp: 0 };
+
+    const lastMessage = messages[messages.length - 1];
+
+    const lastMessageMember = await populateMember(ctx, lastMessage.memberId);
+
+    if (!lastMessageMember) return { count: 0, image: undefined, timestamp: 0 };
+
+    const lastMessageUser = await populateUser(ctx, lastMessageMember.userId);
+
+    return {
+      count: messages.length,
+      image: lastMessageUser?.image,
+      timestamp: lastMessage._creationTime,
+    };
 };
 
 export const create = mutation({
