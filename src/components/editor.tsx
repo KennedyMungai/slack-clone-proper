2658c26e0d65272967e770a02bc3d7b2042d604d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-24 09:20:21 +0300

    Added some classes for quill

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,5 +1,39 @@
+"use client";
+
+import Quill, { type QuillOptions } from "quill";
+import "quill/dist/quill.snow.css";
+import { useEffect, useRef } from "react";
+
 const Editor = () => {
-  return <div>Editor</div>;
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const container = containerRef.current;
+
+    const editorContainer = container.appendChild(
+      container.ownerDocument.createElement("div"),
+    );
+
+    const options: QuillOptions = {
+      theme: "snow",
+    };
+
+    new Quill(editorContainer, options);
+
+    return () => {
+      if (container) container.innerHTML = "";
+    };
+  }, []);
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-col overflow-hidden rounded-md border border-slate-200 bg-white transition focus-within:border-slate-300 focus-within:shadow-sm">
+        <div ref={containerRef} />
+      </div>
+    </div>
+  );
 };
 
 export default Editor;
