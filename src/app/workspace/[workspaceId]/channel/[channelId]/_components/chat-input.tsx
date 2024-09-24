08eb51e08a5dbe28913413e2304e553b4278b964
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-24 14:23:46 +0300

    Augmented the logic used to send messages

--- src/app/workspace/[workspaceId]/channel/[channelId]/_components/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/_components/chat-input.tsx
@@ -5,7 +5,8 @@ import { useChannelId } from "@/hooks/use-channel-id";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import dynamic from "next/dynamic";
 import Quill from "quill";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { toast } from "sonner";
 
 const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 
@@ -16,31 +17,49 @@ type Props = {
 const ChatInput = ({ placeholder }: Props) => {
   const editorRef = useRef<Quill | null>(null);
 
+  const [editorKey, setEditorKey] = useState(0);
+  const [isPending, setIsPending] = useState(false);
+
   const workspaceId = useWorkspaceId();
   const channelId = useChannelId();
 
   const { mutate: createMessage } = useCreateMessage();
 
-  const handleSubmit = ({
+  const handleSubmit = async ({
     body,
     image,
   }: {
     body: string;
     image: File | null;
   }) => {
-    createMessage({
-      body,
-      workspaceId,
-      channelId,
-    });
+    try {
+      setIsPending(true);
+
+      await createMessage(
+        {
+          body,
+          workspaceId,
+          channelId,
+        },
+        { throwError: true },
+      );
+
+      setEditorKey((prevKey) => prevKey + 1);
+    } catch (error) {
+      if (error instanceof Error) toast.error(error.message);
+      else toast.error("Failed to send message");
+    } finally {
+      setIsPending(false);
+    }
   };
 
   return (
     <div className="w-full px-5">
       <Editor
+        key={editorKey}
         onSubmit={handleSubmit}
         placeholder={placeholder}
-        disabled={false}
+        disabled={isPending}
         innerRef={editorRef}
       />
     </div>
