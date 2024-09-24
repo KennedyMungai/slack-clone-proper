fd046d6d202bd63a6af4f6d89e65a12f0b393f84
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-24 09:39:46 +0300

    Created the basic version of the editor

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { ImageIcon, SmileIcon } from "lucide-react";
 import Quill, { type QuillOptions } from "quill";
 import "quill/dist/quill.snow.css";
 import { useEffect, useRef } from "react";
+import { PiTextAa } from "react-icons/pi";
+import { MdSend } from "react-icons/md";
+import Hint from "./hint";
 
 const Editor = () => {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -30,7 +35,53 @@ const Editor = () => {
   return (
     <div className="flex flex-col">
       <div className="flex flex-col overflow-hidden rounded-md border border-slate-200 bg-white transition focus-within:border-slate-300 focus-within:shadow-sm">
-        <div ref={containerRef} />
+        <div ref={containerRef} className="ql-custom h-full" />
+        <div className="z-[5] flex px-2 pb-2">
+          <Hint label="Hide formatting">
+            <Button
+              size="iconSm"
+              disabled={false}
+              variant={"ghost"}
+              onClick={() => {}}
+            >
+              <PiTextAa className="size-4" />
+            </Button>
+          </Hint>
+          <Hint label="Emoji">
+            <Button
+              size="iconSm"
+              disabled={false}
+              variant={"ghost"}
+              onClick={() => {}}
+            >
+              <SmileIcon className="size-4" />
+            </Button>
+          </Hint>
+          <Hint label="Image">
+            <Button
+              size="iconSm"
+              disabled={false}
+              variant={"ghost"}
+              onClick={() => {}}
+            >
+              <ImageIcon className="size-4" />
+            </Button>
+          </Hint>
+
+          <Button
+            className="ml-auto bg-[#007a5a] text-white hover:bg-[#007a5a]/80"
+            size={"iconSm"}
+            disabled={false}
+            onClick={() => {}}
+          >
+            <MdSend className="size-4" />
+          </Button>
+        </div>
+      </div>
+      <div className="flex justify-end p-2 text-[10px] text-muted-foreground">
+        <p>
+          <strong>Shift + Return</strong> to add a new line
+        </p>
       </div>
     </div>
   );
