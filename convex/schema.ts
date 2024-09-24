f71cc8ee22ebf61b79a9afe250638ebdcfa20a68
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-24 15:04:16 +0300

    Added the reactions schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -46,6 +46,15 @@ const schema = defineSchema({
       "parentMessageId",
       "conversationId",
     ]),
+  reactions: defineTable({
+    workspaceId: v.id("workspaces"),
+    messageId: v.id("messages"),
+    memberId: v.id("members"),
+    value: v.string(),
+  })
+    .index("by_workspace_id", ["workspaceId"])
+    .index("by_message_id", ["messageId"])
+    .index("by_member_id", ["memberId"]),
 });
 
 export default schema;
