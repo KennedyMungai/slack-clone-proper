23a8c1d7e600a4e59c11cfe3a04042c9e3bca76e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-25 12:16:40 +0300

    Added some marup at the beginning of every channel

--- src/components/channel-hero.tsx
+++ src/components/channel-hero.tsx
@@ -1,10 +1,20 @@
+import { format } from "date-fns";
+
 type Props = {
-    name: string
-    creationTime: number
+  name: string;
+  creationTime: number;
 };
 
-const ChannelHero = ({creationTime, name}: Props) => {
-  return <div>ChannelHero</div>;
+const ChannelHero = ({ creationTime, name }: Props) => {
+  return (
+    <div className="mx-5 mb-4 mt-[88px]">
+      <p className="mb-2 flex items-center text-2xl font-bold"># {name}</p>
+      <p className="mb-4 font-normal text-slate-800">
+        This channel was created on {format(creationTime, "MMMM do, yyyy")}.
+        This is the very beginning of the <strong>{name}</strong> channel.
+      </p>
+    </div>
+  );
 };
 
 export default ChannelHero;
