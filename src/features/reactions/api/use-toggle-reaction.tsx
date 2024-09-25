38cebaa911ba7969322036d8725d7afafc6d8889
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-25 15:39:13 +0300

    Renamed the hook to toggle reactions

--- src/features/reactions/api/use-toggle-reaction.tsx
+++ src/features/reactions/api/use-toggle-reaction.tsx
@@ -16,7 +16,7 @@ type Options = {
   throwError?: boolean;
 };
 
-export const useCreateReaction = () => {
+export const useToggleReaction = () => {
   const mutation = useMutation(api.reactions.toggle);
 
   const [data, setData] = useState<ResponseType>(null);
