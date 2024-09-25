3a36650ca2205ee416758d68a57a785b491e5ff4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-25 12:02:44 +0300

    Added some markup and styling to the thumbnail component

--- src/components/thumbnail.tsx
+++ src/components/thumbnail.tsx
@@ -1,9 +1,22 @@
+/* eslint-disable @next/next/no-img-element */
+import { Dialog } from "@/components/ui/dialog";
+
 type Props = {
-  url: string;
+  url: string | null | undefined;
 };
 
 const Thumbnail = ({ url }: Props) => {
-  return <div>Thumbnail</div>;
+  if (!url) return null;
+
+  return (
+    <div className="relative my-2 max-w-[360px] cursor-zoom-in overflow-hidden rounded-lg border">
+      <img
+        src={url}
+        alt="Message Image"
+        className="size-full rounded-md object-cover"
+      />
+    </div>
+  );
 };
 
 export default Thumbnail;
