be5e3a9fbbcd139332613e32c5c5d233e346bcc1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-25 11:28:00 +0300

    Added a hint for the non compact markup

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Hint from "@/components/hint";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { format, isToday, isYesterday } from "date-fns";
 import dynamic from "next/dynamic";
 import { Doc, Id } from "../../convex/_generated/dataModel";
@@ -54,16 +55,50 @@ const Message = ({
   updatedAt,
   threadImage,
 }: Props) => {
+  if (isCompact) {
+    return (
+      <div className="group relative flex flex-col gap-2 p-1.5 px-5 hover:bg-gray-100/60">
+        <div className="flex items-start gap-2">
+          <Hint label={formatFullTime(new Date(createdAt))}>
+            <button className="w-[40px] text-center text-xs leading-[22px] text-muted-foreground opacity-0 hover:underline group-hover:opacity-100">
+              {format(new Date(createdAt), "hh:mm")}
+            </button>
+          </Hint>
+        </div>
+        <Renderer value={body} />
+      </div>
+    );
+  }
+
   return (
     <div className="group relative flex flex-col gap-2 p-1.5 px-5 hover:bg-gray-100/60">
       <div className="flex items-start gap-2">
-        <Hint label={formatFullTime(new Date(createdAt))}>
-          <button className="w-[40px] text-center text-xs leading-[22px] text-muted-foreground opacity-0 hover:underline group-hover:opacity-100">
-            {format(new Date(createdAt), "hh:mm")}
-          </button>
-        </Hint>
+        <button>
+          <Avatar className="mr-1">
+            <AvatarImage src={authorImage} alt={authorName} />
+            <AvatarFallback className="bg-sky-500 text-xs text-white">
+              {authorName.charAt(0).toUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+        </button>
+        <div className="flex w-full flex-col overflow-hidden">
+          <div className="text-sm">
+            <button
+              className="font-bold text-primary hover:underline"
+              onClick={() => {}}
+            >
+              {authorName}
+            </button>
+            <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+            <Hint label={formatFullTime(new Date(createdAt))}>
+              <button className="text-xs text-muted-foreground hover:underline">
+                {format(new Date(createdAt), "h:mm a")}
+              </button>
+            </Hint>
+          </div>
+          <Renderer value={body} />
+        </div>
       </div>
-      <Renderer value={body} />
     </div>
   );
 };
