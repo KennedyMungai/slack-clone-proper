6bad1a90fbf1308e9202e38f0f41503c0beb44bd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-25 12:50:28 +0300

    Added some option hints to the toolbar

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -1,3 +1,13 @@
+import { Button } from "@/components/ui/button";
+import {
+  MessageSquareTextIcon,
+  PencilIcon,
+  SmileIcon,
+  TrashIcon,
+} from "lucide-react";
+import Hint from "./hint";
+import EmojiPopover from "@/components/emoji-popover";
+
 type Props = {
   isAuthor: boolean;
   isPending: boolean;
@@ -17,7 +27,35 @@ const Toolbar = ({
   isPending,
   handleReaction,
 }: Props) => {
-  return <div>Toolbar</div>;
+  return (
+    <div className="absolute right-5 top-0">
+      <div className="rounded-md border bg-white opacity-0 shadow-sm transition-opacity group-hover:opacity-100">
+        <EmojiPopover
+          hint="Add reaction"
+          onEmojiSelect={(emoji) => handleReaction(emoji.native)}
+        >
+          <Button variant={"ghost"} size={"icon"} disabled={isPending}>
+            <SmileIcon className="size-5" />
+          </Button>
+        </EmojiPopover>
+        <Hint label={"Reply in thread"}>
+          <Button variant={"ghost"} size={"icon"} disabled={isPending}>
+            <MessageSquareTextIcon className="size-5" />
+          </Button>
+        </Hint>
+        <Hint label={"Edit Message"}>
+          <Button variant={"ghost"} size={"icon"} disabled={isPending}>
+            <PencilIcon className="size-5" />
+          </Button>
+        </Hint>
+        <Hint label={"Delete Message"}>
+          <Button variant={"ghost"} size={"icon"} disabled={isPending}>
+            <TrashIcon className="size-5" />
+          </Button>
+        </Hint>
+      </div>
+    </div>
+  );
 };
 
 export default Toolbar;
