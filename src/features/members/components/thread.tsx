32d03938b4035840445fc343ddce7023e734a117
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-26 11:25:32 +0300

    Added some logic to filter out the editor props

--- src/features/members/components/thread.tsx
+++ src/features/members/components/thread.tsx
@@ -3,12 +3,17 @@
 import Message from "@/components/message";
 import { Button } from "@/components/ui/button";
 import { useCurrentMember } from "@/features/members/api/use-current-member";
+import { useCreateMessage } from "@/features/messages/api/use-create-message";
 import { useGetMessage } from "@/features/messages/api/use-get-message";
+import { useGenerateUploadUrl } from "@/features/upload/api/use-generate-upload-url";
+import { useChannelId } from "@/hooks/use-channel-id";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { LoaderIcon, TriangleAlertIcon, XIcon } from "lucide-react";
-import { useState } from "react";
-import { Id } from "../../../../convex/_generated/dataModel";
 import dynamic from "next/dynamic";
+import Quill from "quill";
+import { useRef, useState } from "react";
+import { toast } from "sonner";
+import { Id } from "../../../../convex/_generated/dataModel";
 
 const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 
@@ -17,10 +22,26 @@ type Props = {
   onClose: () => void;
 };
 
+type CreateMessageValues = {
+  channelId: Id<"channels">;
+  workspaceId: Id<"workspaces">;
+  parentMessageId: Id<"messages">;
+  body: string;
+  image: Id<"_storage"> | undefined;
+};
+
 const Thread = ({ messageId, onClose }: Props) => {
   const workspaceId = useWorkspaceId();
+  const channelId = useChannelId();
 
   const [editingId, setEditingId] = useState<Id<"messages"> | null>(null);
+  const [editorKey, setEditorKey] = useState(0);
+  const [isPending, setIsPending] = useState(false);
+
+  const editorRef = useRef<Quill | null>(null);
+
+  const { mutate: generateUploadUrl } = useGenerateUploadUrl();
+  const { mutate: createMessage } = useCreateMessage();
 
   const { data: message, isLoading: isLoadingMessage } = useGetMessage({
     id: messageId,
@@ -60,6 +81,55 @@ const Thread = ({ messageId, onClose }: Props) => {
     </div>;
   }
 
+  const handleSubmit = async ({
+    body,
+    image,
+  }: {
+    body: string;
+    image: File | null;
+  }) => {
+    try {
+      setIsPending(true);
+      editorRef?.current?.enable(false);
+
+      const values: CreateMessageValues = {
+        channelId,
+        workspaceId,
+        parentMessageId: messageId,
+        body,
+        image: undefined,
+      };
+
+      if (image) {
+        const url = await generateUploadUrl({}, { throwError: true });
+
+        if (!url) throw new Error("Url not found");
+
+        const result = await fetch(url, {
+          method: "POST",
+          headers: { "Content-Type": image.type },
+          body: image,
+        });
+
+        if (!result.ok) throw new Error("Failed to upload image");
+
+        const { storageId } = await result.json();
+
+        values.image = storageId;
+      }
+
+      await createMessage(values, { throwError: true });
+
+      setEditorKey((prevKey) => prevKey + 1);
+    } catch (error) {
+      if (error instanceof Error) toast.error(error.message);
+      else toast.error("Failed to send message");
+    } finally {
+      setIsPending(false);
+      editorRef?.current?.enable(true);
+    }
+  };
+
   return (
     <div className="flex h-full flex-col">
       <div className="flex h-[49px] items-center justify-between border-b px-4">
@@ -86,7 +156,13 @@ const Thread = ({ messageId, onClose }: Props) => {
         />
       </div>
       <div className="px-4">
-        <Editor onSubmit={() => {}} disabled={false} placeholder="Reply...." />
+        <Editor
+          onSubmit={handleSubmit}
+          disabled={isPending}
+          placeholder="Reply...."
+          key={editorKey}
+          innerRef={editorRef}
+        />
       </div>
     </div>
   );
