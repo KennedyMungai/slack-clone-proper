2435395c400098374caee6ddb77295b98eab9fb5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-26 12:24:15 +0300

    Handleed the loading and error cases

--- src/app/workspace/[workspaceId]/member/[memberId]/page.tsx
+++ src/app/workspace/[workspaceId]/member/[memberId]/page.tsx
@@ -1,13 +1,44 @@
 "use client";
 
+import { useCreateOrGetConversation } from "@/features/conversations/api/use-create-or-get-conversation";
 import { useMemberId } from "@/hooks/use-member-id";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { AlertTriangleIcon, LoaderIcon } from "lucide-react";
+import { useEffect } from "react";
 
 const MemberPage = () => {
   const workspaceId = useWorkspaceId();
   const memberId = useMemberId();
 
-  return <div>{JSON.stringify({ workspaceId, memberId })}</div>;
+  const { data, mutate, isPending } = useCreateOrGetConversation();
+
+  useEffect(() => {
+    mutate({
+      workspaceId,
+      memberId,
+    });
+  }, [mutate, memberId, workspaceId]);
+
+  if (isPending) {
+    return (
+      <div className="flex h-full items-center justify-center">
+        <LoaderIcon className="size-5 animate-spin text-muted-foreground" />
+      </div>
+    );
+  }
+
+  if (!data) {
+    return (
+      <div className="flex h-full flex-col items-center justify-center gap-y-2">
+        <AlertTriangleIcon className="size-5 text-muted-foreground" />
+        <span className="text-sm text-muted-foreground">
+          Conversation not found
+        </span>
+      </div>
+    );
+  }
+
+  return <div>{JSON.stringify(data)}</div>;
 };
 
 export default MemberPage;
