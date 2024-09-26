3f0d9863506fe74ff33ad07932c853400d3bcafb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-26 15:07:25 +0300

    Added some logic to the conversation hero compnent

--- src/components/conversation-hero.tsx
+++ src/components/conversation-hero.tsx
@@ -1,10 +1,25 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 type Props = {
   name: string;
   image: string;
 };
 
 const ConversationHero = ({ image, name }: Props) => {
-  return <div>ConversationHero</div>;
+  return (
+    <div className="mx-5 mb-4 mt-[88px]">
+      <div className="mb-2 flex items-center gap-x-1">
+        <Avatar className="mr-2 size-20">
+          <AvatarImage src={image} alt={name} />
+          <AvatarFallback>{name?.charAt(0).toUpperCase()}</AvatarFallback>
+        </Avatar>
+      </div>
+      <p className="text-2xl font-bold">{name}</p>
+      <p className="mb-4 font-normal text-slate-800">
+        This conversation is just between you and <strong>{name}</strong>
+      </p>
+    </div>
+  );
 };
 
 export default ConversationHero;
