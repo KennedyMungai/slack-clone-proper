c248d74b08a2c6706a653f32b192adbe64859e7b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-26 15:41:39 +0300

    Added some markup and logic to the threadbar component

--- src/components/thread-bar.tsx
+++ src/components/thread-bar.tsx
@@ -1,12 +1,28 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 type Props = {
   count?: number;
   image?: string;
   timestamp?: number;
-  onClick?: () => void;
+  onClick: () => void;
 };
 
-const ThreadBar = ({ count, timestamp, image }: Props) => {
-  return <div>ThreadBar</div>;
+const ThreadBar = ({ count, timestamp, image, onClick }: Props) => {
+  if (!count || !timestamp) return null;
+
+  return (
+    <button
+      onClick={onClick}
+      className="group/thread-bar flex max-w-[600px] items-center justify-start rounded-md border border-transparent p-1 transition hover:border-border hover:bg-white"
+    >
+      <div className="flex items-center gap-2 overflow-hidden">
+        <Avatar className="shrink-0">
+          <AvatarImage src={image} />
+          <AvatarFallback>M</AvatarFallback>
+        </Avatar>
+      </div>
+    </button>
+  );
 };
 
 export default ThreadBar;
