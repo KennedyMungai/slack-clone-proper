14f1623135b482e65b7f888d0753c61e7d2bc3f5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-26 09:30:00 +0300

    Using the react intersection observer to load more messages

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -2,12 +2,13 @@
 
 import ChannelHero from "@/components/channel-hero";
 import Message from "@/components/message";
+import { useCurrentMember } from "@/features/members/api/use-current-member";
 import { GetMessagesReturnType } from "@/features/messages/api/use-get-messages";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { differenceInMinutes, format, isToday, isYesterday } from "date-fns";
 import { useState } from "react";
 import { Id } from "../../convex/_generated/dataModel";
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { useCurrentMember } from "@/features/members/api/use-current-member";
+import { LoaderIcon } from "lucide-react";
 
 type Props = {
   channelName?: string;
@@ -107,6 +108,33 @@ const MessageList = ({
           })}
         </div>
       ))}
+      <div
+        className="h-1"
+        ref={(el) => {
+          if (el) {
+            const observer = new IntersectionObserver(
+              ([entry]) => {
+                if (entry.isIntersecting && canLoadMore) {
+                  loadMore();
+                }
+              },
+              { threshold: 1.0 },
+            );
+
+            observer.observe(el);
+
+            return () => observer.disconnect();
+          }
+        }}
+      />
+      {isLoadingMore && (
+        <div className="relative my-2 text-center">
+          <hr className="absolute left-0 right-0 top-1/2 border-t border-gray-300" />
+          <span className="relative inline-block rounded-full border border-gray-300 bg-white px-4 py-1 text-xs shadow-sm">
+            <LoaderIcon className="size-5 animate-spin text-muted-foreground" />
+          </span>
+        </div>
+      )}
       {variant === "channel" && channelName && channelCreationTime && (
         <ChannelHero name={channelName} creationTime={channelCreationTime} />
       )}
