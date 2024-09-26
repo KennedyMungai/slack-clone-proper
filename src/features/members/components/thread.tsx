89e53e8a452c04b476fbad73d97afb4fc39744fe
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-26 10:33:27 +0300

    Handled the loading and error states of the thread

--- src/features/members/components/thread.tsx
+++ src/features/members/components/thread.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { XIcon } from "lucide-react";
-import { Id } from "../../../../convex/_generated/dataModel";
 import { Button } from "@/components/ui/button";
+import { useGetMessage } from "@/features/messages/api/use-get-message";
+import { LoaderIcon, TriangleAlertIcon, XIcon } from "lucide-react";
+import { Id } from "../../../../convex/_generated/dataModel";
 
 type Props = {
   messageId: Id<"messages">;
@@ -10,6 +11,33 @@ type Props = {
 };
 
 const Thread = ({ messageId, onClose }: Props) => {
+  const { data: message, isLoading: isLoadingMessage } = useGetMessage({
+    id: messageId,
+  });
+
+  if (isLoadingMessage) {
+    return (
+      <div className="flex h-full items-center justify-center">
+        <LoaderIcon className="size-5 animate-spin text-muted-foreground" />
+      </div>
+    );
+  }
+
+  if (!message) {
+    <div className="flex h-full flex-col">
+      <div className="flex h-[49px] items-center justify-between border-b px-4">
+        <p className="text-lg font-bold">Thread</p>
+        <Button onClick={onClose} size={"iconSm"} variant={"ghost"}>
+          <XIcon className="size-5 stroke-[1.5]" />
+        </Button>
+      </div>
+      <div className="flex h-full flex-col items-center justify-center gap-y-2">
+        <TriangleAlertIcon className="size-5 text-muted-foreground" />
+      </div>
+      <p className="text-sm text-muted-foreground">Message not found</p>
+    </div>;
+  }
+
   return (
     <div className="flex h-full flex-col">
       <div className="flex h-[49px] items-center justify-between border-b px-4">
