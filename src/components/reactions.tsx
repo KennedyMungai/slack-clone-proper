98c9b39aced6f64c92126834bcca17265736aa34
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-26 08:33:35 +0300

    Using data instead of reactions for the Reactions props

--- src/components/reactions.tsx
+++ src/components/reactions.tsx
@@ -1,19 +1,16 @@
 import { Doc, Id } from "../../convex/_generated/dataModel";
 
 type Props = {
-  reactions: Array<
-    Omit<
-      Doc<"reactions">,
-      "memberId" & {
-        count: number;
-        memberIds: Id<"members">[];
-      }
-    >
+  data: Array<
+    Omit<Doc<"reactions">, "memberId"> & {
+      count: number;
+      memberIds: Id<"members">[];
+    }
   >;
   onReactionChange: (value: string) => void;
 };
 
-const Reactions = ({ onReactionChange, reactions }: Props) => {
+const Reactions = ({ onReactionChange, data }: Props) => {
   return <div>Reactions</div>;
 };
 
