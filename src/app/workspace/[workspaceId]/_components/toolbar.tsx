44e906193c134e73912f9148b2b0cf2d08832a8f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-27 15:41:18 +0300

    Using the command component to show the channels and the members

--- src/app/workspace/[workspaceId]/_components/toolbar.tsx
+++ src/app/workspace/[workspaceId]/_components/toolbar.tsx
@@ -1,14 +1,31 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "@/components/ui/command";
+import { useGetChannels } from "@/features/channels/api/use-get-channels";
+import { useGetMembers } from "@/features/members/api/use-get-members";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { InfoIcon, SearchIcon } from "lucide-react";
+import { useState } from "react";
 
 const Toolbar = () => {
   const workspaceId = useWorkspaceId();
 
   const { data } = useGetWorkspace({ id: workspaceId });
+
+  const { data: channels } = useGetChannels({ workspaceId });
+  const { data: members } = useGetMembers({ workspaceId });
+
+  const [open, setOpen] = useState(false);
 
   return (
     <nav className="flex h-10 items-center justify-between bg-[#481349] p-1.5">
@@ -16,11 +33,29 @@ const Toolbar = () => {
       <div className="max-[642px] min-w-[280px] shrink grow-[2]">
         <Button
           size={"sm"}
+          onClick={() => setOpen(true)}
           className="h-7 w-full justify-start bg-accent/25 px-2 hover:bg-accent/25"
         >
           <SearchIcon className="mr-2 size-4 text-white" />
           <span className="text-xs text-white">Search {data?.name}</span>
         </Button>
+        <CommandDialog open={open} onOpenChange={() => setOpen(false)}>
+          <CommandInput placeholder="Type a command or search..." />
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup heading="Channels">
+              {channels?.map((channel) => (
+                <CommandItem key={channel._id}>{channel.name}</CommandItem>
+              ))}
+            </CommandGroup>
+            <CommandSeparator />
+            <CommandGroup heading="Members">
+              {members?.map((member) => (
+                <CommandItem key={member._id}>{member.user.name}</CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </CommandDialog>
       </div>
       <div className="ml-auto flex flex-1 items-center justify-end">
         <Button variant={"transparent"} size={"iconSm"}>
