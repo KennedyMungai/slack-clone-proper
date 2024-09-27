ef93d5f917be289c2017e800cbf69d126433f9d9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-27 12:17:37 +0300

    Added the markup for the loading and error states

--- src/features/members/components/profile.tsx
+++ src/features/members/components/profile.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useGetMember } from "@/features/members/api/use-get-member";
+import { LoaderIcon, TriangleAlertIcon, XIcon } from "lucide-react";
 import { Id } from "../../../../convex/_generated/dataModel";
 
 type Props = {
@@ -5,8 +10,52 @@ type Props = {
   onClose: () => void;
 };
 
-const Profile = ({memberId, onClose}: Props) => {
-  return <div>Profile</div>;
+const Profile = ({ memberId, onClose }: Props) => {
+  const { data: member, isLoading: isLoadingMember } = useGetMember({
+    id: memberId,
+  });
+
+  if (isLoadingMember) {
+    return (
+      <div className="flex h-full flex-col">
+        <div className="flex h-[49px] items-center justify-between border-b px-4">
+          <p className="text-lg font-bold">Profile</p>
+          <Button onClick={onClose} size={"iconSm"} variant={"ghost"}>
+            <XIcon className="size-5 stroke-[1.5]" />
+          </Button>
+        </div>
+        <div className="flex h-full items-center justify-center">
+          <LoaderIcon className="size-5 animate-spin text-muted-foreground" />
+        </div>
+      </div>
+    );
+  }
+
+  if (!member) {
+    <div className="flex h-full flex-col">
+      <div className="flex h-[49px] items-center justify-between border-b px-4">
+        <p className="text-lg font-bold">Profile</p>
+        <Button onClick={onClose} size={"iconSm"} variant={"ghost"}>
+          <XIcon className="size-5 stroke-[1.5]" />
+        </Button>
+      </div>
+      <div className="flex h-full flex-col items-center justify-center">
+        <TriangleAlertIcon className="size-5 text-muted-foreground" />
+        <p className="text-sm text-muted-foreground">Profile not found</p>
+      </div>
+    </div>;
+  }
+
+  return (
+    <div>
+      <div className="flex h-[49px] items-center justify-between border-b px-4">
+        <p className="text-lg font-bold">Profile</p>
+        <Button onClick={onClose} size={"iconSm"} variant={"ghost"}>
+          <XIcon className="size-5 stroke-[1.5]" />
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
