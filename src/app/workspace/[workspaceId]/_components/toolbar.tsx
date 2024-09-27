a1726c5b66396899e43e4afc70559ba24f916c92
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-27 15:51:41 +0300

    Using onSelect instead of onClick

--- src/app/workspace/[workspaceId]/_components/toolbar.tsx
+++ src/app/workspace/[workspaceId]/_components/toolbar.tsx
@@ -15,10 +15,13 @@ import { useGetMembers } from "@/features/members/api/use-get-members";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { InfoIcon, SearchIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 const Toolbar = () => {
   const workspaceId = useWorkspaceId();
+
+  const router = useRouter();
 
   const { data } = useGetWorkspace({ id: workspaceId });
 
@@ -26,6 +29,11 @@ const Toolbar = () => {
   const { data: members } = useGetMembers({ workspaceId });
 
   const [open, setOpen] = useState(false);
+
+  const onChannelClick = (url: string) => {
+    setOpen(false);
+    router.push(url);
+  };
 
   return (
     <nav className="flex h-10 items-center justify-between bg-[#481349] p-1.5">
@@ -39,19 +47,37 @@ const Toolbar = () => {
           <SearchIcon className="mr-2 size-4 text-white" />
           <span className="text-xs text-white">Search {data?.name}</span>
         </Button>
-        <CommandDialog open={open} onOpenChange={() => setOpen(false)}>
+        <CommandDialog open={open} onOpenChange={setOpen}>
           <CommandInput placeholder="Type a command or search..." />
           <CommandList>
             <CommandEmpty>No results found.</CommandEmpty>
             <CommandGroup heading="Channels">
               {channels?.map((channel) => (
-                <CommandItem key={channel._id}>{channel.name}</CommandItem>
+                <CommandItem
+                  key={channel._id}
+                  onSelect={() =>
+                    onChannelClick(
+                      `/workspace/${workspaceId}/channel/${channel._id}`,
+                    )
+                  }
+                >
+                  {channel.name}
+                </CommandItem>
               ))}
             </CommandGroup>
             <CommandSeparator />
             <CommandGroup heading="Members">
               {members?.map((member) => (
-                <CommandItem key={member._id}>{member.user.name}</CommandItem>
+                <CommandItem
+                  key={member._id}
+                  onSelect={() =>
+                    onChannelClick(
+                      `/workspace/${workspaceId}/member/${member._id}`,
+                    )
+                  }
+                >
+                  {member.user.name}
+                </CommandItem>
               ))}
             </CommandGroup>
           </CommandList>
